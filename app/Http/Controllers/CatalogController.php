7db9b067f386230ef5190208dfa4ce13472e5aa0
Aleksey Polyakov <sozdaibk@gmail.com>
2020-10-31 13:23:29 +0300

    фильтрация

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -6,25 +6,99 @@ use Illuminate\Http\Request;
 use App\Models\Item;
 use App\Models\AttrGroup;
 
-
 class CatalogController extends Controller
 {
     //
 
-    public function list(){
+    public function list(Request $request){
 
 
-        $items = Item::with('attrs')->get();
-        $groups = AttrGroup::with('attrs')->get();
 
+        $data = $request->validate([
+            'priceFrom' => 'numeric|min:0',
+            'priceTo' => 'numeric|min:0',
+        ]);
+
+
+        // Доступные группы фильтров
+        $groups = AttrGroup::with('attrs')->get(["id", "title", "slug"])->toArray();
+
+
+
+        // Обработаем get запрос, и сравнив с доступными фильтрами
+        $input_filter = [];
+        foreach($groups as &$group){
+
+            foreach($group['attrs'] as &$filter_attr){
+                $filter_attr['checked'] = false;
+            }
+
+            if($request->has($group['slug'])){
+                if(!empty($request->get($group['slug']))){
+                    $input_filter_array = preg_split("/[\s,]+/is", $request->get($group['slug'])) ?? [];
+
+                    foreach($group['attrs'] as &$filter_attr){
+                        $filter_attr['checked'] = in_array($filter_attr['slug'], $input_filter_array);
+                    }
+
+                    $input_filter[$group['slug']] = $input_filter_array;
+                }
+                
+            }
+        }
+
+        
+        // Загрузим коллекцию товаров
+        $items = new Item();
+
+        // Если есть фильтры свойств, то применим
+        foreach($input_filter as $filters){
+            if(!empty($filters)){
+                $items = $items->whereHas('attrs', function ($query) use ($filters) {
+                    $query->where(function($q) use ($filters){
+                        foreach($filters as $filter){
+                            $q->orWhere("slug", $filter);
+                        }
+                    });
+                });
+            }
+        }
+
+
+        // Запомним минимальные и максимальные цены на основе фильтров по свойствам
+        $price_min = $items->min('price') ?? 0;
+        $price_max = $items->max('price') ?? 9000;
+
+
+        // если есть фильтры цен, то применим
+        if($request->has("priceTo")){
+            $items = $items->where("price", "<=", $data["priceTo"]);
+        }
+
+        if($request->has("priceFrom")){
+            $items = $items->where("price", ">=", $data['priceFrom']);
+        }
+
+        // Подтянем свойства товаров, на всякий случай я их вывожу в карточке товара
+        $items = $items->with('attrs');
+
+        
+        
+        
         $filter_data = [
             "groups" => $groups,
-            "price_min" => 0, //Item::min('price'),
-            "price_max" => Item::max('price')
+            "price_min" => $price_min,
+            "price_max" => $price_max,
+            "price_filter" => [
+                empty($data['priceFrom']) ? $price_min : $data['priceFrom'], 
+                empty($data['priceTo']) ? $price_max : $data['priceTo']
+            ]
         ];
+        
+        
 
         return view('catalog',[
-            'items' => $items,
+            'items' => $items->get(),
             'filter' => $filter_data
         ]);
     }
--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -4,14 +4,14 @@
     @foreach ($filter['groups'] as $group)
         <!-- filter-item -->
     <div class="filter-item">
-        <div class="filter-title">{{$group->title}}</div>
+        <div class="filter-title">{{$group['title']}}</div>
         <div class="filter-content">
             <ul class="filter-list">
 
-                @foreach ($group->attrs as $attr)
+                @foreach ($group['attrs'] as $attr)
                 <li>
-                    <input type="checkbox" id="filter-{{$group->slug}}-{{$attr->slug}}">
-                    <label for="filter-{{$group->slug}}-{{$attr->slug}}">{{$attr->title}}</label>
+                    <input type="checkbox" data-group="{{$group['slug']}}" data-attr="{{$attr['slug']}}" id="filter-{{$group['slug']}}-{{$attr['slug']}}" {{$attr['checked'] ? 'checked' : ''}}>
+                    <label for="filter-{{$group['slug']}}-{{$attr['slug']}}">{{$attr['title']}}</label>
                 </li>
                 @endforeach
             </ul>
@@ -24,24 +24,92 @@
         <div class="filter-title">Цена</div>
         <div class="filter-content">
             <div class="price">
-                <input type="text" class="price-input ui-slider-min" value="{{$filter['price_min']}}">
+                <input type="text" class="price-input ui-slider-min" value="{{$filter['price_filter'][0]}}">
                 <span class="price-sep"></span>
-                <input type="text" class="price-input ui-slider-max" value="{{$filter['price_max']}}">
+                <input type="text" class="price-input ui-slider-max" value="{{$filter['price_filter'][1]}}">
             </div>
             <div class="ui-slider"></div>
+
+
+
             <script>
+
                $('document').ready(function () {
+
+
+                // По-хорошему нужно было создать отдельный массив, который можно сделать публичным/
+                // И указать там свойства без айдишников и тд
+                let config = @json($filter)
+
+                function changeFilters(){
+
+                    let o = {}
+
+
+                    let priceFrom = parseFloat($('.ui-slider-min').val());
+                    let priceTo = parseFloat($('.ui-slider-max').val())
+
+                    if(priceFrom != config.price_min){
+                        o['priceFrom'] = priceFrom;
+                    }
+
+                    if(priceTo != config.price_max){
+                        o['priceTo'] = priceTo;
+                    }
+                    
+
+                    $('.filter-list').find('input:checked').each(function(){
+
+                        let group = $(this).attr('data-group')
+                        let attr = $(this).attr('data-attr')
+
+                        if(!o[group]){
+                            o[group] = []
+                        }
+
+                        o[group].push(attr);
+                        
+
+                    })
+
+
+                    var query = Object.keys(o)
+                    .map(k => k + '=' + (typeof o[k] == "array" ? o[k].join(',') : o[k]))
+                    .join('&');
+
+                    location.href = '/catalog?' + query;
+                }
+
+                $('.filter-list').find('input').change(function(e){
+                    let el = $(this);
+                    console.log("change", el.attr('data-group'), el.attr('data-attr'))
+
+
+                    changeFilters();
+                });
+
+                $('.clear_filter').click(function(){
+                    location.href = '/catalog'
+                });
+
+
                   $('.ui-slider').slider({
                      animate: false,
                      range: true,
-                     values: [0, {{$filter['price_max']}}],
-                     min: 0,
-                     max: {{$filter['price_max']}},
+                     values: [config.price_filter[0], config.price_filter[1]],
+                     min: config.price_min,
+                     max: config.price_max,
                      step: 0.01,
                      slide: function (event, ui) {
                          if (ui.values[1] - ui.values[0] < 1) return false;
                          $('.ui-slider-min').val(ui.values[0]);
                          $('.ui-slider-max').val(ui.values[1]);
+
+
+                         
+                     },
+                     change: function(event, ui){
+                        changeFilters();
                      }
                   });
                });
@@ -51,7 +119,7 @@
     <!-- filter-item -->
     <div class="filter-item">
         <div class="filter-content">
-            <button class="btn">Сбросить фильтр</button>
+            <button class="btn clear_filter">Сбросить фильтр</button>
         </div>
     </div>
 </div>
